94b658107cb683ebdd1024bf9f35fdef0332e9aa
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-02-24 22:47:34 +0100

    updated test

--- tests/index.test.js
+++ tests/index.test.js
@@ -4,14 +4,20 @@ const app = require("../routes/index");
 describe("GET /", () => {
     it("responds with status 200", async () => {
         const response = await request(app).get("/");
-        expect(response.status).toBe(200);
+        expect(response.statusCode).toBe(200);
     });
 });
 
 describe("GET /contact", () => {
     it("responds with status 200", async () => {
         const response = await request(app).get("/contact");
-        expect(response.status).toBe(200);
+        expect(response.statusCode).toBe(200);
     });
 });
 
+describe("GET /about", () => {
+    it("responds with status 200", async () => {
+        const response = await request(app).get("/about");
+        expect(response.status).toBe(200);
+    });
+});
