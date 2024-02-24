47c7802ad6fa6ba3c8705f4fde7e60281206b4e2
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-02-24 15:03:02 +0100

    Dodat automatski slider

--- public/javascript/slider.js
+++ public/javascript/slider.js
@@ -1,15 +1,26 @@
 let slideIndex = 0;
 
 function showSlides() {
-    let i;
     let slides = document.querySelectorAll(".slide");
-    console.log(slides[slideIndex]);
-    for (i = 0; i < slides.length; i++) {
+    for (let i = 0; i < slides.length; i++) {
         slides[i].classList.add("hidden");
     }
     if (slideIndex > slides.length - 1) { slideIndex = 0; }
     slides[slideIndex].classList.remove("hidden");
     slideIndex++;
+
+    let progressBars = document.querySelectorAll(".progressBar");
+    progressBars.forEach(progressBar => {
+        let currentWidth = 0;
+        let interval = setInterval(() => {
+            if (currentWidth >= 100) {
+                clearInterval(interval);
+            } else {
+                currentWidth += 1;
+                progressBar.style.width = `${currentWidth}%`;
+            }
+        }, 35);
+    });
 
     setTimeout(showSlides, 4000);
 }
