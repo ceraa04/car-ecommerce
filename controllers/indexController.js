766825ed4f2be33248f8bf0914f7ced309f86de0
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-02-25 18:59:36 +0100

    dodata funkcija za pojedinacan auto

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,18 +1,64 @@
-const Car = require('../models/Car')
+const Car = require("../models/Car");
+const Brand = require("../models/Brand");
 
-// Display all cars function
 
-const cars_displayAll = (req, res) => {
-    Car.find()
-        .then((result) => {
-            console.log("Number of cars retreived is: " + result.length)
-            res.render('index', {
-                title: 'Car Shop',
-                cars: result
-            })
+const carOfTheWeek = async () => {
+    try {
+        const carsSorted = await Car.find().sort({ price: -1 }).populate("brand");
+        const popularCar = carsSorted[0];
+        return popularCar;
+
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+
+const add_car = (req, res) => {
+    const newCar = new Car({
+        model: "M5",
+        price: 140000,
+        category: "limo",
+        brand: "65d89d530b4fff2c81525bc2",
+        description: "The BMW M5 is an icon among enthusiasts, renowned for its fusion of track-ready performance and everyday usability. Its sleek exterior design hints at the power within, boasting aggressive lines, aerodynamic enhancements, and signature M badging. Under the hood lies a potent engine, typically a turbocharged inline-six, delivering exhilarating acceleration and a thrilling exhaust note. ",
+        year: 2021
+    });
+
+    newCar.save()
+        .then(savedCar => {
+            console.log("New car with embedded brand information saved:", savedCar);
+            res.redirect("/");
         })
-        .catch((err) => console.error(err))
-}
+        .catch(error => {
+            console.error("Error saving car:", error);
+        });
 
+};
+const cars_imgSlider = async (req, res) => {
+    const popularCar = await carOfTheWeek();
+    Car.find()
+        .limit(4)
+        .populate("brand")
+        .then((result) => {
+            res.render("index", {
+                cars: result,
+                popularCar: popularCar
+            });
+        })
+        .catch((err) => console.error(err));
 
-module.exports = cars_displayAll
+};
+
+const findCarPage = async (req, res, id) => {
+    try {
+        const wantedCar = await Car.findOne({ _id: id });
+        res.send(wantedCar);
+    } catch (error) {
+        console.error(error);
+    }
+};
+module.exports = {
+    cars_imgSlider,
+    add_car,
+    carOfTheWeek,
+    findCarPage
+};
