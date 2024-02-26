52cbad7675a6299548a63e41827542bd49bf3a87
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-02-26 16:09:24 +0100

    Spojeno sortiranje i filtriranje

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,6 @@
 const Car = require("../models/Car");
 const Brand = require("../models/Brand");
 
-
 const carOfTheWeek = async () => {
     try {
         const carsSorted = await Car.find().sort({ price: -1 }).populate("brand");
@@ -9,6 +8,22 @@ const carOfTheWeek = async () => {
         return popularCar;
 
     } catch (error) {
+        console.log(error.message);
+    }
+};
+
+const getAllCars = async (req, res) => {
+    try {
+        const cars = await Car.find()
+            .populate("brand");
+        res.render("products", {
+            cars: cars,
+            // Select option za sortiranje kada se stranica tek ucitava, kad i dalje nije POST method vec GET
+            selectedOptionFilter: "",
+            selectedOptionSort: ""
+        });
+    }
+    catch (error) {
         console.log(error.message);
     }
 };
@@ -48,10 +63,48 @@ const cars_imgSlider = async (req, res) => {
 
 };
 
-const findCarPage = async (req, res, id) => {
+const filterAndSortCars = async (req, res) => {
+    // Ovo su vrednosti koje se nalaze u SELECT inputima u trenutku submitovanja forme
+    const sortValue = req.body.sortMethod;
+    const filterValue = req.body.filterMethodBrand;
+
+    // Sortiranje
+    let sortMethod;
+    if (sortValue === "ascendingPrice") {
+        sortMethod = { price: 1 };
+    }
+    else if (sortValue === "descendingPrice") {
+        sortMethod = { price: -1 };
+    }
+    else if (sortValue === "descendingYear") {
+        sortMethod = { year: -1 };
+    }
+    else if (sortValue === "ascendingYear") {
+        sortMethod = { year: 1 };
+    }
+    // Filtriranje
+    let filterMethodBrand;
+    if (filterValue !== "none") {
+        const brand = await Brand.findOne({ name: filterValue });
+        filterMethodBrand = { brand: brand._id };
+    }
+    Car.find(filterMethodBrand).populate("brand").sort(sortMethod)
+        .then((result) => {
+            res.render("products", {
+                cars: result,
+                selectedOptionSort: sortValue,
+                selectedOptionFilter: filterValue
+            });
+        })
+        .catch(err => {
+            console.error(err);
+        });
+};
+
+const singleCarPage = async (req, res, id) => {
     try {
-        const wantedCar = await Car.findOne({ _id: id });
-        res.send(wantedCar);
+        const car = await Car.findOne({ _id: id }).populate("brand");
+        res.render("itemPage", { car: car });
     } catch (error) {
         console.error(error);
     }
@@ -60,5 +113,7 @@ module.exports = {
     cars_imgSlider,
     add_car,
     carOfTheWeek,
-    findCarPage
+    singleCarPage,
+    getAllCars,
+    filterAndSortCars
 };
