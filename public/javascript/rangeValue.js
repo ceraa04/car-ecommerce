cd43938c249af911fb601d1b19b0221c7d702ef7
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-02-28 12:01:49 +0100

    Stavljen toFixed(0) za rangeValue.js da bi bio ceo broj

--- public/javascript/rangeValue.js
+++ public/javascript/rangeValue.js
@@ -1,9 +1,13 @@
 const rangeInput = document.querySelector(".rangeInputFilter");
-
-rangeInput.addEventListener("mousemove", (event) => {
+function updateTargetPrice() {
     const targetPrice = document.querySelector(".targetPrice");
     const maxPrice = parseInt(document.querySelector(".maxPrice").textContent);
     const minPrice = parseInt(document.querySelector(".minPrice").textContent);
-    targetPrice.textContent = minPrice + (event.target.value / 100) * (maxPrice - minPrice);
+    const rangeInput = document.querySelector("input[type='range']");
 
-});
+    targetPrice.textContent = (minPrice + (rangeInput.value / 100) * (maxPrice - minPrice)).toFixed(0);
+}
+
+rangeInput.addEventListener("mousemove", updateTargetPrice);
+
+window.addEventListener("load", updateTargetPrice);
