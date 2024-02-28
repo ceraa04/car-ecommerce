7dc584fb8b5b191b7b8e13f4c121b55b3dfae532
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-02-28 23:42:48 +0100

    update Test

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,16 +1,45 @@
 const { getAllBrands } = require("../controllers/indexController");
 const Brand = require("../models/Brand");
+const Car = require("../models/Car");
 
 jest.mock("../models/Brand");
+jest.mock("../models/Car");
+
 
 describe("getAllBrands", () => {
-    it("returns an array of brand names", async () => {
+    it("returns an array of brand objects with all properties", async () => {
         // Mock Brand model behavior
-        const brands = [{ name: "Toyota" }, { name: "Honda" }];
-        require("../models/Brand").find.mockResolvedValue(brands);
+        const brands = [
+            {
+                name: "Toyota",
+                madeIn: 1910,
+                description: "random description",
+                url: "https://github.com/"
+            },
+            {
+                name: "Honda",
+                madeIn: 1910,
+                description: "random description",
+                url: "https://github.com/"
+            }
+        ];
+        Brand.find.mockResolvedValue(brands);
 
         const result = await getAllBrands();
-        expect(result).toEqual(["Toyota", "Honda"]);
-    });
 
+        expect(result).toEqual([
+            {
+                name: "Toyota",
+                madeIn: 1910,
+                description: "random description",
+                url: "https://github.com/"
+            },
+            {
+                name: "Honda",
+                madeIn: 1910,
+                description: "random description",
+                url: "https://github.com/"
+            }
+        ]);
+    });
 });
