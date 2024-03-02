223c36cdf602022d35c3906f792f2fd78b5c6298
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-02 22:13:36 +0100

    Dodat error page za 404, onemogucen pristup editovanju bazi, sem ako nije ulogovan admin

--- routes/app.js
+++ routes/app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
+
 const path = require("path");
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 const mongoose = require("mongoose");
 require("dotenv").config();
 const carController = require("../public/controllers/indexController");
 const passport = require("./auth");
 const session = require("express-session");
 const flash = require("connect-flash");
+// Routes
 const authRouter = require("./authRoutes");
 const editCarsRouter = require("./editCarsRouter");
 const productsRouter = require("./productsRouter");
@@ -16,7 +20,6 @@ const indexPageRouter = require("./indexPageRouter");
 
 app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, "..", "public")));
 const mongoDB = process.env.DB_CONNECTION_STRING;
 
 mongoose.connect(mongoDB)
@@ -36,6 +39,9 @@ app.use(session({
 }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
+
+// Middleware za definisanje user-a koji je trenutno prijavljen 
+//za svaku stranicu
 app.use((req, res, next) => {
   res.locals.currentUser = req.user;
   next();
@@ -67,4 +73,10 @@ app.use("/newBrand", newBrandRouter);
 app.use("/editCars", editCarsRouter);
 // Autentikacija korisnika
 app.use("/", authRouter);
+app.get("*", async (req, res) => {
+  res.render("errorPage", {
+    cars: await carController.getAllCars(),
+
+  });
+});
 module.exports = app;
