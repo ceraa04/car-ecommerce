39afe9673d68ebbf2e9637ef0f2d5a6574342513
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-05 12:07:56 +0100

    update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -25,12 +25,58 @@ router.get("/signout", (req, res, next) => {
 
 // Sign-up route
 router.get("/signup", (req, res) => {
-    res.render("signup");
+    res.render("signup", {
+        messages: {
+            error: req.flash("error")
+        }
+    });
 });
 
 router.post("/signup", async (req, res, next) => {
     try {
         // Salt je broj dodatnih random karaktera koje ce bcrypt funkcija dodati
+        const existingUser = await User.findOne({ username: req.body.username });
+        if (existingUser) {
+            req.flash("error", "Username already exists");
+            res.redirect("/signup");
+            return;
+        }
+        else if (req.body.password.length < 4) {
+            req.flash("error", "Password has to have at least 4 letters");
+            res.redirect("/signup");
+            return;
+        }
+        else if (req.body.password.toLowerCase() === req.body.username.toLowerCase()) {
+            req.flash("error", "Password cannot be the same as username!");
+            res.redirect("/signup");
+            return;
+        }
+        else if (req.body.password !== req.body.repeatedPassword) {
+            req.flash("error", "Passwords must match");
+            res.redirect("/signup");
+            return;
+        }
+        let hasUpperCase = false;
+        let hasLowerCase = false;
+
+        for (let i = 0; i < req.body.password.length; i++) {
+            if (req.body.password[i] === req.body.password[i].toUpperCase()) {
+                hasUpperCase = true;
+            } else if (req.body.password[i] === req.body.password[i].toLowerCase()) {
+                hasLowerCase = true;
+            }
+
+            // Ako su oba slucaja pronadjena, izadji iz loopa
+            if (hasUpperCase && hasLowerCase) {
+                break;
+            }
+        }
+
+        if (!hasUpperCase || !hasLowerCase) {
+            req.flash("error", "Password has to have at least 1 uppercase and 1 lowercase letter");
+            return res.redirect("/signup");
+        }
+
         const salt = await bcrypt.genSalt(15);
         const hashedPassword = await bcrypt.hash(req.body.password, salt);
         const user = new User({
@@ -38,7 +84,7 @@ router.post("/signup", async (req, res, next) => {
             password: hashedPassword
         });
         await user.save();
-        res.redirect("/");
+        res.redirect("/signin");
     } catch (err) {
         return next(err);
     }
