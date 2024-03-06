82535c9f5b96fcec638092abf2872ec36817db48
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-06 11:46:28 +0100

    Index page, nav and footer completely designed responsively

--- routes/newCarRouter.js
+++ routes/newCarRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const dbItemsController = require("../controllers/getAllController");
-
+const adminController = require("../controllers/adminController");
 router.get("/", async (req, res) => {
     res.render("newCar", {
         brands: await dbItemsController.getAllBrands()
@@ -9,7 +9,7 @@ router.get("/", async (req, res) => {
 });
 router.post("/", async (req, res) => {
     const { model, price, year, description, brand } = req.body;
-    await carController.add_car(model, price, year, description, brand);
+    await adminController.add_car(model, price, year, description, brand);
     res.redirect("/editCars");
 });
 
