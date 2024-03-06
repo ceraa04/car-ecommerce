a994e24bf9fb1113ec6cb091ff141d00ce6d9dab
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-06 12:24:53 +0100

    User profile positioning in nav.ejs fixed for every breakpoint

--- routes/newBrandRouter.js
+++ routes/newBrandRouter.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const adminController = require("../controllers/adminController");
 
 router.get("/", async (req, res) => {
     res.render("newBrand");
@@ -8,7 +9,7 @@ router.get("/", async (req, res) => {
 router.post("/", async (req, res) => {
     const { brand, founded, description, urlNewBrand } = req.body;
     if (brand) {
-        await carController.add_brand(req, res, brand, founded, description, urlNewBrand);
+        await adminController.add_brand(req, res, brand, founded, description, urlNewBrand);
         res.redirect("/newBrand");
     } else {
         console.log("Neispravne vrednosti za add brand!");
