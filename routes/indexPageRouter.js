1992ff9ce6ca05682a54de8fbda6f7a480febff5
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-07 18:59:19 +0100

    Removed res.locals.cars from indexpage too

--- routes/indexPageRouter.js
+++ routes/indexPageRouter.js
@@ -5,7 +5,6 @@ const indexPageController = require("../controllers/indexPageController");
 router.get("/", async (req, res) => {
     res.render("index", {
         imgSliderCars: await indexPageController.cars_imgSlider(),
-        cars: res.locals.cars,
         popularCar: await indexPageController.carOfTheWeek(),
     });
 });
